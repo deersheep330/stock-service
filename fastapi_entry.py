26063b9958e978431a7919ad41ecc93843c66d7d
yang chiu <deersheep330@gmail.com>
2021-07-06 14:30:07 +0800

    refine fastapi

--- fastapi_entry.py
+++ fastapi_entry.py
@@ -1,24 +1,43 @@
-from typing import Optional
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from stock.trend_chart import PttTrendChart, ReunionTrendChart
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/ptt")
 def ptt():
     ptt_trend_chart = PttTrendChart()
     print(ptt_trend_chart.trends)
-    return ptt_trend_chart.trends
+    return ptt_trend_chart.trends[:8]
 
 
 @app.get("/reunion")
 def reunion():
     reunion_trend_chart = ReunionTrendChart()
     print(reunion_trend_chart.trends)
-    return reunion_trend_chart.trends
+    return reunion_trend_chart.trends[:8]
+
+
+@app.get("/ins-buy")
+def ins_buy():
+    return []
+
+
+@app.get("/ins-sell")
+def ins_sell():
+    return []
 
 
 @app.get("/health_check")
